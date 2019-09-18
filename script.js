755755adfed1ff232c4842d508b8b5fbd7bc1642
Lorien Davis <lozid123@googlemail.com>
2019-09-18 19:39:38 +1000

    Change site to look more aesthetic

---
+++ script.js
@@ -0,0 +1,41 @@
+const nameText = document.querySelector('h1');
+const endText = document.querySelector('h2');
+const button = document.querySelector('span');
+// const shopButton = document.querySelector('a');
+const names = {
+  0: ['Shanti', 'instagram.com/shanti_woah'],
+  1: ['Alysse', 'instagram.com/alysseburford'],
+  2: ['Jahvis', 'instagram.com/jahvisloveday'],
+  3: ['Jason', 'instagram.com/jason_howarth'],
+  4: ['Lorien', 'instagram.com/lorien.lorien']
+}
+const namesLength = Object.keys(names).length;
+
+function joinNames(names) {
+  let joinedNames = '';
+
+  for (let i = 0; i < namesLength; i += 1) {
+    if (i < namesLength - 2) {
+      joinedNames += `${names[i][0]}, `
+    } else if (i == namesLength - 2) {
+      joinedNames += `${names[i][0]} `;
+    } else {
+      joinedNames += `& ${names[i][0]}`;
+    }
+  }
+
+  return joinedNames;
+}
+
+function changeText(names) {
+  nameText.innerHTML = names;
+  endText.textContent = 'own urbooty.';
+}
+
+button.addEventListener('click', function () {
+  const joinedNames = joinNames(names);
+  changeText(joinedNames);
+  // shopButton.classList.add('visible');
+});
+
+
