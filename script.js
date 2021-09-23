252ed5970751264a2d377c792d25f7d335578c29
Lorien <lozid123@gmail.com>
2021-09-24 08:58:40 +1000

    Change display of urbooty.com

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 const nameText = document.querySelector("h1");
 const endText = document.querySelector("h2");
+const freeloaderText = document.querySelector(".freeloaders");
 const button = document.querySelector("span");
-// const shopButton = document.querySelector('a');
+// const shopButton = document.querySelector("a");
 const owners = {
-  0: ["Lorien", "instagram.com/lorien.lorien"],
+  0: { name: "Lorien", link: "instagram.com/lorien.lorien" },
 };
 
 const freeloaders = {
-  0: ["Shanti", "instagram.com/shanti_woah"],
-  1: ["Alysse", "instagram.com/alysseburford"],
-  2: ["Jahvis", "instagram.com/jahvisloveday"],
+  0: { name: "Shanti", link: "instagram.com/shanti_woah" },
+  1: { name: "Alysse", link: "instagram.com/alysseburford" },
+  2: { name: "Jahvis", link: "instagram.com/jahvisloveday" },
 };
 
-const namesLength = Object.keys(names).length;
-
 function joinNames(names) {
+  const namesLength = Object.keys(names).length;
   let joinedNames = "";
+
+  if (namesLength == 1) return names[0].name;
 
   for (let i = 0; i < namesLength; i += 1) {
     if (i < namesLength - 2) {
-      joinedNames += `${names[i][0]}, `;
+      joinedNames += `${names[i].name}, `;
     } else if (i == namesLength - 2) {
-      joinedNames += `${names[i][0]} `;
+      joinedNames += `${names[i].name} `;
     } else {
-      joinedNames += `& ${names[i][0]}`;
+      joinedNames += `& ${names[i].name}`;
     }
   }
 
@@ -38,5 +40,6 @@ function changeText(names) {
 button.addEventListener("click", function () {
   const joinedNames = joinNames(owners);
   changeText(joinedNames);
-  // shopButton.classList.add('visible');
+  freeloaderText.style.display = "block";
+  // shopButton.classList.add("visible");
 });
